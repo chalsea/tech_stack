262ee6011468eff4bc33ed449bf3cfce5acc3908
Chalsea Chen <chalsea@creatabu.com>
2017-03-20 11:23:13 -0700

    making react redux library list rendering work

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation
+} from 'react-native';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDescription(){
+    const { library, expanded } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text style={styles2.descriptionStyle}>
+            {library.description}
+          </Text>
+        </CardSection>
+      );
+    }
+  }
+  //Step 2, replace <Text style={{ flex: 1 }}> with <Text style={styles2.descriptionStyle}>
+
   render() {
     const { titleStyle } = styles;
     const { id, title } = this.props.library;
@@ -19,6 +43,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
   );
@@ -32,4 +57,19 @@ const styles = {
   }
 };
 
-export default connect(null, actions)(ListItem);
+const styles2 = {
+  descriptionStyle: {
+    flex: 1,
+    margin: 5,
+    paddingLeft: 15
+  }
+};
+// Step 1: add in styles2 so it's not duplicated as style for titleStyle above
+
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
